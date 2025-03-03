6a60370f42dd03f56b15d29b1e4cf56f950cc7b7
anthony-y-zhu14 <anthonyzhu1114@gmail.com>
2025-03-03 13:29:37 -0500

    - support multiple entry

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,42 @@
 import typescript from '@rollup/plugin-typescript'
 
+const PLUGINS = [
+	typescript({
+		compilerOptions: {declaration: true, noEmit: false},
+		include: ['src/**/*'],
+		exclude: ['**/*.{test,spec}.{ts,js}']
+	})
+]
+
+const INPUT = ['src/index.ts']
+
 /** @type {import('rollup').RollupOptions} */
 const ESM_CONFIG = {
-	input: 'src/index.ts',
+	input: INPUT,
 	output: {
-		file: 'lib/index.js',
+		dir: 'lib',
+		entryFileNames: '[name].js',
+		preserveModules: true,
+		preserveModulesRoot: 'src',
 		format: 'esm',
 		sourcemap: true
 	},
-	plugins: [
-		typescript({
-			compilerOptions: {declaration: true, noEmit: false},
-            include: ['src/**/*'],
-			exclude: ['**/*.{test,spec}.{ts,js}']
-		})
-	]
+	plugins: PLUGINS
 }
 
 /** @type {import('rollup').RollupOptions} */
 const CJS_CONFIG = {
-	input: 'src/index.ts',
+	input: INPUT,
 	output: {
-		file: 'lib/index.cjs',
+		dir: 'lib',
+		entryFileNames: '[name].cjs',
+		preserveModules: true,
+		preserveModulesRoot: 'src',
 		format: 'commonjs',
-        exports: 'named',
+		exports: 'named',
 		sourcemap: true
 	},
-	plugins: [
-		typescript({
-			compilerOptions: {declaration: true, noEmit: false},
-            include: ['src/**/*'],
-			exclude: ['**/*.{test,spec}.{ts,js}']
-		})
-	]
+	plugins: PLUGINS
 }
 
 export default [
